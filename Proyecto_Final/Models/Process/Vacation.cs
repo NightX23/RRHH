565eed2baa7a81a8a5b265e537a3fcbe34342510
Marlon <marlon231098@hotmail.com>
2019-04-07 10:29:28 -0400

    Add Vacation Entity for Process Module

---
+++ Proyecto_Final/Models/Process/Vacation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Models.Process
+{
+    public class Vacation
+    {
+
+        public int Id { get; set; }
+        //FK EMPLOYEES ----------------------------------------------------------------------------
+        public int EmployeeId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+        //-----------------------------------------------------------------------------------------
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Desde")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hasta")]
+        public DateTime EndDate { get; set; }
+        [Display(Name ="Comentario")]
+        public string comment { get; set; }
+    }
+}
